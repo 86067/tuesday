9a1ad443173f36c32c29d1191d8a557f03b8d8b9
86067 <86067@MHS-C212-19.mentorschools.org>
2017-11-07 11:41:28 -0500

    Added Friday, added colon to Wednesday

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
 print "Tuesday: Cloudy, High 46, Low 32"
-print "Wednesday, Partly Cloudy, High 46, Low 32"
+print "Wednesday: Partly Cloudy, High 46, Low 32"
 print "Thursday: Rainy, High: 48, Low 32"
+print "Friday: Cloudy, High 31 Low 22"
